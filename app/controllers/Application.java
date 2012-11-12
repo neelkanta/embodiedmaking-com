d163d716fd848814568a525202a541798e2e802d
Andre Kampert <andre@cadimira.com>
2012-11-12 17:24:33 +0100

    Coming soon placeholder

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,9 +6,24 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+
+    public static Result index() {
+        return ok(index.render());
+    }
+
+    public static Result ventures() {
+        return ok(comingSoon.render("ventures"));
+    }
+
+    public static Result method() {
+        return ok(comingSoon.render("method"));
+    }
+
+    public static Result vision() {
+        return ok(comingSoon.render("vision"));
+    }
+
+    public static Result contact() {
+        return ok(comingSoon.render("contact"));
+    }
 }
