39d57b548865da843ace0e3f56ad4e4e8c37b2e9
Andre Kampert <andre@cadimira.com>
2012-12-17 12:49:20 +0100

    Indicate maturity on pattern overview

--- app/models/Pattern.scala
+++ app/models/Pattern.scala
@@ -5,13 +5,14 @@ import java.io.InputStream
 
 import scala.collection.JavaConversions._
 
-case class Pattern(id: String = "",
-                   title: String = "",
-                   summary: String = "",
-                   problem: String = "",
-                   solution: String = "",
-                   patternLanguage: String = "",
-                   synonyms: List[String] = Nil)
+case class Pattern(id: String,
+                   maturity: Int,
+                   title: String,
+                   summary: String,
+                   problem: String,
+                   solution: String,
+                   patternLanguage: String,
+                   synonyms: List[String])
 
 object Pattern {
   def all(): List[Pattern] = {
@@ -25,14 +26,18 @@ object Pattern {
     try {
       val scanner: Scanner = new Scanner(stream, "UTF-8").useDelimiter("\n\n")
 
-      val title: String = if (scanner.hasNext) scanner.next() else ""
+      val caption = if (scanner.hasNext) scanner.next() else ""
+      val split: Array[String] = caption.split('|')
+
+      val title: String = if (split.length > 0) split(0) else ""
+      val maturity: Int = if (split.length > 1) split(1).toInt else 2
       val summary: String = if (scanner.hasNext) scanner.next() else ""
       val problem: String = if (scanner.hasNext) scanner.next() else "to be continued..."
       val solution: String = if (scanner.hasNext) scanner.next() else "to be continued..."
       val patternLanguage: String = if (scanner.hasNext) scanner.next() else "to be continued..."
       val synonyms: List[String] = if (scanner.hasNext) title :: scanner.next().split(",").toList else List(title)
 
-      new Pattern(id, title, summary, problem, solution, patternLanguage, synonyms)
+      new Pattern(id, maturity, title, summary, problem, solution, patternLanguage, synonyms)
     } finally {
       stream.close()
     }
