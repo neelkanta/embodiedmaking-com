49e9ceafcc1cb90c1438e0c7f1b66fead38c3959
Andre Kampert <andre@cadimira.com>
2013-01-17 20:46:50 +0100

    Logged in users can comment using facebook

--- app/controllers/RequestInfo.scala
+++ app/controllers/RequestInfo.scala
@@ -3,11 +3,11 @@ package controllers
 import securesocial.core.Identity
 import securesocial.core.SecureSocial
 
-case class RequestInfo(user: Option[Identity])
+case class RequestInfo(user: Option[Identity], uri: String)
 
 trait ProvidesRequestInfo extends SecureSocial {
   implicit def requestInfo[A](implicit request: RequestWithUser[A]): RequestInfo = {
-    RequestInfo(request.user)
+    RequestInfo(request.user, request.uri)
   }
 }
 
