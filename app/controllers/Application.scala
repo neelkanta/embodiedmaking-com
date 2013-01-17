c1c737c59ca3aebcd74ebe5fea0ad8670a61e134
Andre Kampert <andre@cadimira.com>
2013-01-17 21:39:47 +0100

    Ugly hack to keep the previous uri for returning after login

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,46 +2,51 @@ package controllers
 
 import play.api.mvc._
 import models._
+import securesocial.core.SecureSocial
 
 object Application extends Controller with ProvidesRequestInfo {
 
   def index = UserAwareAction { implicit request =>
-    Ok(views.html.index())
+    Ok(views.html.index()).withSession(storeUri(request))
   }
 
   def ventures = UserAwareAction { implicit request =>
-    Ok(views.html.comingSoon("ventures"))
+    Ok(views.html.comingSoon("ventures")).withSession(storeUri(request))
   }
 
   def method = UserAwareAction { implicit request =>
-    Ok(views.html.method(MethodSection.all()))
+    Ok(views.html.method(MethodSection.all())).withSession(storeUri(request))
   }
 
   def methodSection(id: String) = UserAwareAction { implicit request =>
     try {
-      Ok(views.html.methodSection(MethodSection.all(), MethodSection.get(id), MethodSlide.all(id)))
+      Ok(views.html.methodSection(MethodSection.all(), MethodSection.get(id), MethodSlide.all(id))).withSession(storeUri(request))
     } catch {
-      case e: Exception => Redirect("/method")
+      case e: Exception => Redirect("/method").withSession(storeUri(request))
     }
   }
 
   def patterns = UserAwareAction { implicit request =>
-    Ok(views.html.patterns(Pattern.all()))
+    Ok(views.html.patterns(Pattern.all())).withSession(storeUri(request))
   }
 
   def pattern(id: String) = UserAwareAction { implicit request =>
     try {
-      Ok(views.html.pattern(Pattern.all(), Pattern.get(id)))
+      Ok(views.html.pattern(Pattern.all(), Pattern.get(id))).withSession(storeUri(request))
     } catch {
-      case e: Exception => Redirect("/patterns")
+        case e: Exception => Redirect("/patterns").withSession(storeUri(request))
     }
   }
 
   def vision = UserAwareAction { implicit request =>
-    Ok(views.html.comingSoon("vision"))
+    Ok(views.html.comingSoon("vision")).withSession(storeUri(request))
   }
 
   def contact = UserAwareAction { implicit request =>
-    Ok(views.html.comingSoon("contact"))
+    Ok(views.html.comingSoon("contact")).withSession(storeUri(request))
+  }
+
+  private def storeUri(request: Request[Any]) : Session = {
+    request.session + (SecureSocial.OriginalUrlKey -> request.uri)
   }
 }
