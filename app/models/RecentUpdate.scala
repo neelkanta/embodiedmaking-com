93e2b266c0e3cf6978160ccfddb85b7298c5068c
Andre Kampert <andre@cadimira.com>
2013-02-13 00:05:16 +0100

    Disable recent updates for now

--- app/models/RecentUpdate.scala
+++ app/models/RecentUpdate.scala
@@ -13,25 +13,26 @@ import play.api.{Configuration, Play}
 case class RecentUpdate(title: String, date: LocalDateTime)
 
 object RecentUpdate {
-  private val recentUpdates: List[RecentUpdate] = {
-    val configuration: Configuration = Play.application.configuration
-    val url: URL = new URL(configuration.getString("venturesite.recentUpdates.source").get)
-    val urlConnection: URLConnection = url.openConnection()
-
-    val input: SyndFeedInput = new SyndFeedInput()
-    val reader: XmlReader = new XmlReader(urlConnection)
-
-    try {
-      val feed: SyndFeed = input.build(reader)
-      val entries: List[SyndEntry] = feed.getEntries.toList.asInstanceOf[List[SyndEntry]]
-
-      entries.take(configuration.getInt("venturesite.recentUpdates.count").getOrElse(6)).map(entry => {
-        new RecentUpdate(entry.getTitle, new LocalDateTime(entry.getUpdatedDate))
-      })
-    } finally {
-      reader.close()
-    }
-  }
+  private val recentUpdates: List[RecentUpdate] = List()
+// {
+//    val configuration: Configuration = Play.application.configuration
+//    val url: URL = new URL(configuration.getString("venturesite.recentUpdates.source").get)
+//    val urlConnection: URLConnection = url.openConnection()
+//
+//    val input: SyndFeedInput = new SyndFeedInput()
+//    val reader: XmlReader = new XmlReader(urlConnection)
+//
+//    try {
+//      val feed: SyndFeed = input.build(reader)
+//      val entries: List[SyndEntry] = feed.getEntries.toList.asInstanceOf[List[SyndEntry]]
+//
+//      entries.take(configuration.getInt("venturesite.recentUpdates.count").getOrElse(6)).map(entry => {
+//        new RecentUpdate(entry.getTitle, new LocalDateTime(entry.getUpdatedDate))
+//      })
+//    } finally {
+//      reader.close()
+//    }
+//  }
 
   def all(): List[RecentUpdate] = recentUpdates
 }
